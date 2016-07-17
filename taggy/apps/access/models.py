b190654189f05c03ade171830fb2e1f36e428181
michel-cf <michel@creatingfuture.eu>
2016-07-17 15:45:48 +0200

    Admin for company

--- taggy/apps/access/models.py
+++ taggy/apps/access/models.py
@@ -8,6 +8,10 @@ class Company(models.Model):
     code = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+
 
 class Team(models.Model):
     code = models.CharField(max_length=255)
