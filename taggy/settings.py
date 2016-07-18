6bf552c79b97037a4bd8995732239c38610f0210
michel-cf <michel@creatingfuture.eu>
2016-07-18 17:55:50 +0200

    Possibly required settings

--- taggy/settings.py
+++ taggy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggy',
     'taggy.apps.resource_types.vault',
     'taggy.apps.resource_types.pages',
     'taggy.apps.tagging',
@@ -59,8 +60,7 @@ ROOT_URLCONF = 'taggy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # TO CHECK if needed
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,5 +128,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+# TO CHECK if needed
+STATIC_ROOT = "/var/www/taggy/static/"
 
 STATIC_URL = '/static/'
